a005a49e94730cc1c6eee14f357d296244fc2739
Andrew Tachilin <andrewtachilin@gmail.com>
2018-04-12 23:15:23 +0300

    fix

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_CATALOG', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'W:\domains\opencart/admin/');
+define('DIR_SYSTEM', 'W:\domains\opencart/system/');
+define('DIR_DATABASE', 'W:\domains\opencart/system/database/');
+define('DIR_LANGUAGE', 'W:\domains\opencart/admin/language/');
+define('DIR_TEMPLATE', 'W:\domains\opencart/admin/view/template/');
+define('DIR_CONFIG', 'W:\domains\opencart/system/config/');
+define('DIR_IMAGE', 'W:\domains\opencart/image/');
+define('DIR_CACHE', 'W:\domains\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'W:\domains\opencart/download/');
+define('DIR_LOGS', 'W:\domains\opencart/system/logs/');
+define('DIR_CATALOG', 'W:\domains\opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'W:\domains\opencart/catalog/');
+define('DIR_SYSTEM', 'W:\domains\opencart/system/');
+define('DIR_DATABASE', 'W:\domains\opencart/system/database/');
+define('DIR_LANGUAGE', 'W:\domains\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'W:\domains\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'W:\domains\opencart/system/config/');
+define('DIR_IMAGE', 'W:\domains\opencart/image/');
+define('DIR_CACHE', 'W:\domains\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'W:\domains\opencart/download/');
+define('DIR_LOGS', 'W:\domains\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
